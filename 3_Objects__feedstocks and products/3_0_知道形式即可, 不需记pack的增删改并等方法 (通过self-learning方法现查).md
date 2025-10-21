ea5c4231765a04e77b332ae17eba8008ba04dc15
wang890 <wisecash@126.com>
2025-10-21 21:58:12 +0800

    Chapter Objects improvement

---
+++ 3_Objects__feedstocks and products/3_0_知道形式即可, 不需记pack的增删改并等方法 (通过self-learning方法现查).md
@@ -0,0 +1,44 @@
+3_7节，待学完 chapter 4 后再回来学习。
+
+### .1 objects：feedstock and product of Function machine
++ 单个的且初级的 single and primary: None, Number, Bool
++ 单个的且自定义 single: self-defined by `class`，类的部件、部件的元件都可能是自定义的类
+
++ 非单个的一堆的 pack and iterable, 五种：str字符串, list列表, tuple元组, set集合, dict字典
+
++ 有序的 ordered pack: str, list, tuple
++ 无序的 unordered pack: set, dict
+
++ 可修改的 mutable pack: list, set, dict
++ 不可修改的 imtable pack: str, tuple
++ 有序且可修改的 ordered and mutable pack: list
+
+上述 objects 满足了所有应用场景，选用何种类型的 feedstock object, 直接影响其处理函数算法的复杂性。
+
+### .2 程序和算法
++ 程序就是 feedstock objects → Machine processes → product objects；
++ Machine processes，由operator运算符、function函数、class.function类的函数组成，他们都可称为算法；
++ 算法：就是 处理步骤 或者 系列处理步骤 Machine processes；
++ 算法的封装：语句表达式expr → 函数fenction → 类lib → 库lib → 软件software。
+
+
+### .3 pack
++ 由一堆元素孩子组成，元素element 之间用**英文逗号分隔**，字典的元素是 键值对`key:value`；
++ pack可以无限嵌套，但嵌套多的pack，其处理函数写起来复杂；
+
++ 形式：str `''` or `""`, list `[]`, tuple `()`, set `{}`, dict `{key1:value1, key2:value2, ...}`;
++ 形式，特殊：空的 set `set()`, 只有1个 元素element 的tuple `(ele,)`;
+
++ 五种pack都可迭代iterable，dict在迭代时默认以 key为主；
++ 文中的 iterable 即指 str, list, tuple, set, dict 这五种当中的任何一种；
++ 文中的 mutalbe  即指 list, set, dict 这三种当中的任何一种。
+
+### .4 如何学习 pack
++ 知道 五种pack 的形式即可；
++ 虽然教程代码包括 pack的增删改并 等方法，通过 self-learning方法 现查，如 `pack.`后IDE(编程软件，集成开发环境)就能显示有哪些方法函数；
++ 有某种场景需求时，可以按场景百度搜索。
+
+### .5 Python语言特点
++ 用tab缩进体现语句代码之间的层级关系；
++ 除了缩进，其他地方的空格可以是多个 (有时可以利用这个特点做对齐)；
++ 体现语法的标点符号均是英文标点，标点符号后可以留0至多个空格，规范的形式为留一个空格 (行的末尾时不留空格)。
--- 3_Objects__feedstocks and products/3_7_single_and_self-defined__one example to master class.py
+++ 3_Objects__feedstocks and products/3_7_single_and_self-defined__one example to master class.py
@@ -8,11 +8,11 @@ class Human(object):
 
 
 # 所有的self表示当前实例, 可以全部替换成其他词语 如this, 有的语言就是this
-class Student(Human):  # 类,模板
-    total = 0  # 共性 类变量
+class Student(Human):          # 类,模板
+    total = 0                  # 共性 类变量
 
     def __init__(self, name):  # 实例方法, 具体来说 这个init函数是初始化函数即构造函数
-        self.univ = 'NCEPU'  # 实例变量
+        self.univ = 'NCEPU'    # 实例变量
         self.__class__.total += 1
         super(Student, self).__init__(name)  # 由于继承有父类，父类Human中有构造函数, 在此要调用父类的构造函数
         print(self.name)
@@ -21,10 +21,10 @@ class Student(Human):  # 类,模板
     def func():
         pass
 
-    def chi_fan(self):  # 实例(对象)函数, self 实例化后的具体的某个Student, 对象
+    def chi_fan(self):     # 实例(对象)函数, self 实例化后的具体的某个Student, 对象
         print(self.name + '拿了标有"' + self.name + '"的饭盒') # 这个是个性化的 用了个体实例的名字，所以此函数为实例函数
 
-    @classmethod  # 类方法，共用 这个类能够使用的方法
+    @classmethod           # 类方法，共用 这个类能够使用的方法
     def kai_ban_hui(cls):  # cls可以替换成其他词语，指代的是这个类Student
         cls.total += 0
         print('开班会是所有学生的事情，是共性，不是某个学生的个性')
@@ -32,16 +32,16 @@ class Student(Human):  # 类,模板
 
 if __name__ == '__main__':
 
-    yebing = Student('yebing') # 实例化一个学生，以模板Student()造出一位学生
-    yebing.chi_fan()
-    yebing.kai_ban_hui()
+    Zhangfei = Student('Zhangfei') # 实例化一个学生，以模板Student()造出一位学生
+    Zhangfei.chi_fan()
+    Zhangfei.kai_ban_hui()
     Student.kai_ban_hui()
 
     print('total='+str(Student.total))
 
-    hairui = Student('hairui')
-    hairui.chi_fan()
-    hairui.kai_ban_hui()
+    Guanerye = Student('Guanerye')
+    Guanerye.chi_fan()
+    Guanerye.kai_ban_hui()
     Student.kai_ban_hui()
 
     print('total='+str(Student.total))
