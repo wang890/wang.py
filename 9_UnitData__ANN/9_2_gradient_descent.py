ae4399497427c3d7188f4f8c2be8576c37e2317f
wang890 <wisecash@126.com>
2025-12-17 12:24:20 +0800

    gradient.md to latex

--- 9_UnitData__ANN/9_2_gradient_descent.py
+++ 9_UnitData__ANN/9_2_gradient_descent.py
@@ -1,7 +1,7 @@
 # 用梯度下降算法求函数最小值：L=x1^2 +2x2^2
 import matplotlib.pyplot as plt
 
-m = 0.1 # 0.001, 0.1, 1, 1不行 会形成振荡不收敛
+lmd = 0.1 # 0.001, 0.1, 1, 1不行 会形成振荡不收敛 (\lambda)
 x1 = -10  # 1
 x2 = 30  # 3
 L = x1 ** 2 + 2 * x2 ** 2
@@ -11,8 +11,8 @@ threshold = 0.0000000001
 loss_deltas = []
 
 while loss_delta > threshold and n < 2000:
-    x1 = x1 - 2 * m * x1  # 迭代
-    x2 = x2 - 4 * m * x2  # 迭代
+    x1 = x1 - 2 * lmd * x1  # 迭代
+    x2 = x2 - 4 * lmd * x2  # 迭代
     loss_delta = abs(x1 ** 2 + 2 * x2 ** 2 - L)
     # 计算前后两次迭代后函数差值的绝对值
     loss_deltas.append(loss_delta)
